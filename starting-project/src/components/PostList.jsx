6870126692edf4afd44eb3c3a649fe4026e5bf5c
Santosh Kumar <santoshku8287@gmail.com>
2025-05-10 21:31:11 +0530

    React Router integrated with fetch post and submit post

--- starting-project/src/components/PostList.jsx
+++ starting-project/src/components/PostList.jsx
@@ -1,65 +1,28 @@
-import { useEffect, useState } from "react";
 import Post from "./Post";
-import NewPost from './NewPost';
-import Modal from './Modal';
 import classes from './PostList.module.css';
+import { useLoaderData } from "react-router-dom";
 
 
-function PostList({isPosting, onStopPosting}){
-    const [posts, setPosts] = useState([]);
-    const [isFetching, setIsFetching] = useState(false);
-
-    function addPostHandler(postData){
-        fetch('http://localhost:8080/posts', {
-            method: 'POST',
-            body: JSON.stringify(postData),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        setPosts((existingPosts) => [postData, ...existingPosts]);
-    }
-
-    useEffect(() => {
-        async function fetchPosts(){
-            setIsFetching(true)
-            const response = await fetch('http://localhost:8080/posts');
-            const resData = await response.json();
-            setPosts(resData.posts)
-            setIsFetching(false)
-        }
-
-        fetchPosts();
-    },[])
-
+function PostList(){
+    const posts = useLoaderData();
     return (
         <>
             {
-                isPosting &&
-                    <Modal onClose={onStopPosting}>
-                        <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}/>
-                    </Modal>  
-            }   
-            {
-                !isFetching && posts.length > 0 && (
+                posts.length > 0 && (
                     <ul className={classes.post}>
                         {posts.map(post => <Post key={post.id} author={post.author} body={post.body}/>)}
                     </ul>
                 )
             }
             {
-                !isFetching && posts.length === 0 && (
+                posts.length === 0 && (
                     <div style={{textAlign: 'center', color: 'white'}}>
                         <h2>There are not posts yet.</h2>
                         <p>Start adding some!</p>
                     </div>
                 )
             }
-            {isFetching && (
-                <div style={{textAlign: 'center', color: 'white'}}>
-                    <p>Loading posts...</p>
-                </div>
-            )}
+            
         </>
     )
 }
--- starting-project/src/main.jsx
+++ starting-project/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Posts, {loader as postLoader} from './routes/Posts'
 import './index.css'
+import NewPost, {action as newPostAction} from './routes/NewPost'
+import RootLayout from './routes/RootLayout'
 
+const router = createBrowserRouter([
+  { path: '/', element: <RootLayout/>, 
+    children:[
+        { path: '/', 
+          element: <Posts/>, 
+          loader: postLoader,
+          children:
+          [{ path: '/create-post', element: <NewPost/>, action: newPostAction}],
+        },
+    ]},
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 )
